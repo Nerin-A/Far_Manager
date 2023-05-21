8f5748c4e3972843a647942d6ead8fdab972e0cf
700grains <60423788+700grains@users.noreply.github.com>
2023-05-21 23:57:12 +0300

    Add an X_Pos position

--- Asm_Tools.asm
+++ Asm_Tools.asm
@@ -28,16 +28,21 @@ Draw_Line_Horizontal proc
 
 	; 1. Calculate the address to output a character: addres_offset = (x_y_pos.Y_Pos * x_y_pos.Screen_width + x_y_pos.X_Pos) * 4 (4 bytes for 1 symbol)
 	; 1.1 Calculating x_y_pos.Y * x_y_pos.Screen_Width
-	mov rax, edx
+	mov rax, rdx
 	shr rax, 16 ; AX = x_y_pos.Y_Pos
 	movzx rax, ax ; RAX = AX = x_y_pos.Y_Pos
 
 
-	mov rbx, edx
+	mov rbx, rdx
 	shr rbx, 32 ; BX = x_y_pos.Screen_width
 	movzx rbx, bx ; RBX = BX = x_y_pos.Screen_width
 
 	imul rax, rbx ; RAX = RAX * RBX = x_y_pos.Y_Pos * x_y_pos.Screen_Width
+
+	; 1.2 Add x_y_pos.X_Pos to RAX
+	movzx rbx, dx ; RBX = DX = x_y_pos.X_Pos
+	add rax, rbx ; RAX = x_y_pos.Y_Pos * x_y_pos.Screen_Width + x_y_pos.X_Pos
+
 
 	; 2. Show symbols
 	mov rdi, rcx
