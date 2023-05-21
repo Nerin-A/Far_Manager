974547bd33844143c16a513f0ac27b4e178a1586
700grains <60423788+700grains@users.noreply.github.com>
2023-05-22 00:51:42 +0300

    Let's move the code to Get_Pos_Address() to avoid copy-paste

--- Asm_Tools.asm
+++ Asm_Tools.asm
@@ -14,18 +14,11 @@ Make_Sum proc
 
 Make_Sum endp
 ;------------------------------------------------------------------------------------------------------------
-Draw_Line_Horizontal proc
-; extern "C" void Draw_Line_Horizontal (CHAR_INFO* screen_buffer, XYPos pos, CHAR_INFO symbol);
+Get_Pos_Address proc
 ; Parameters:
 ; RCX = screen_buffer
 ; RDX = pos
-; R8 = symbol
-; Return NONE
-	
-	push rax
-	push rbx
-	push rcx
-	push rdi
+; Return RDI
 
 	; 1. Calculate the address to output a character: addres_offset = (x_y_pos.Y_Pos * x_y_pos.Screen_width + x_y_pos.X_Pos) * 4 (4 bytes for 1 symbol)
 	; 1.1 Calculating x_y_pos.Y * x_y_pos.Screen_Width
@@ -48,6 +41,25 @@ Draw_Line_Horizontal proc
 
 	mov rdi, rcx ; RDI = screen_buffer
 	add rdi, rax ; RDI = screen_buffer + addres_offset
+
+	ret
+
+Get_Pos_Address endp
+;------------------------------------------------------------------------------------------------------------
+Draw_Line_Horizontal proc
+; extern "C" void Draw_Line_Horizontal (CHAR_INFO* screen_buffer, XYPos pos, CHAR_INFO symbol);
+; Parameters:
+; RCX = screen_buffer
+; RDX = pos
+; R8 = symbol
+; Return NONE
+	
+	push rax
+	push rbx
+	push rcx
+	push rdi
+
+	call Get_Pos_Address
 
 	; 2. Show symbols
 	mov eax, r8d
