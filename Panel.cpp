30402e1f8db56ebdc42fa6c07ddb2e61d5405e6d
700grains <60423788+700grains@users.noreply.github.com>
2023-05-22 23:39:42 +0300

    Add the panel constructor

--- Panel.cpp
+++ Panel.cpp
@@ -1,1 +1,13 @@
 #include "Panel.h"
+// APanel
+//------------------------------------------------------------------------------------------------------------
+APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height)
+:X_Pos(x_pos), Y_Pos(y_pos), Width(width), Height(height)
+{
+
+}
+void APanel::Draw()
+{
+
+}
+//------------------------------------------------------------------------------------------------------------
