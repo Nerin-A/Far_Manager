86db722e8d496cf3c0a7b08144b0c5d10557af79
700grains <60423788+700grains@users.noreply.github.com>
2023-05-22 23:57:12 +0300

    Pass the screen buffer to the constructor

--- Panel.cpp
+++ Panel.cpp
@@ -1,8 +1,8 @@
 #include "Panel.h"
 // APanel
 //------------------------------------------------------------------------------------------------------------
-APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height)
-	:X_Pos(x_pos), Y_Pos(y_pos), Width(width), Height(height)
+APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height, CHAR_INFO* screen_buffer, unsigned short screen_width)
+	:X_Pos(x_pos), Y_Pos(y_pos), Width(width), Height(height), Screen_Buffer(screen_buffer), Screen_Width(screen_width)
 {
 
 }
@@ -11,11 +11,11 @@ void APanel::Draw()
    CHAR_INFO symbol{};
    symbol.Char.UnicodeChar = L'X';
    symbol.Attributes = 0x1b;
-   XYPos x_y_pos(2, 1, screen_buffer_info.dwSize.X, 10);
+   XYPos x_y_pos(2, 1, Screen_Width, 10);
 
-   //Draw_Line_Horizontal(screen_buffer, x_y_pos, symbol);
-   Draw_Line_Vertical(screen_buffer, x_y_pos, symbol);
+   //Draw_Line_Horizontal(Screen_Buffer, x_y_pos, symbol);
+   Draw_Line_Vertical(Screen_Buffer, x_y_pos, symbol);
 
-   //Show_Colors(screen_buffer, x_y_pos, symbol);
+   //Show_Colors(Screen_Buffer, x_y_pos, symbol);
 }
 //------------------------------------------------------------------------------------------------------------
--- Panel.h
+++ Panel.h
@@ -19,12 +19,14 @@ struct XYPos
 class APanel
 {
 public:
-   APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height);
+   APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height, CHAR_INFO* screen_buffer, unsigned short screen_width);
 
    void Draw();
 
    unsigned short X_Pos, Y_Pos;
    unsigned short Width, Height;
+   unsigned short Screen_Width;
+   CHAR_INFO* Screen_Buffer;
 };
 //------------------------------------------------------------------------------------------------------------
 extern "C" int Make_Sum(int value_1, int value_2);
