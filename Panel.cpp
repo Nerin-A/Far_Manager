ff23370cdfefe50ad72ae723dee30a92704550e7
700grains <60423788+700grains@users.noreply.github.com>
2023-05-28 20:29:27 +0300

    Draw vertical lines

--- Panel.cpp
+++ Panel.cpp
@@ -11,7 +11,7 @@ void APanel::Draw()
 	CHAR_INFO horizontal_symbol{};
 	CHAR_INFO vertical_symbol{};
 	XYPos horizontal_pos(1, 0, Screen_Width, Width - 2);
-	XYPos vertical_pos(1, 0, Screen_Width, Width - 2);
+	XYPos vertical_pos(0, 1, Screen_Width, Height - 2);
 
 	horizontal_symbol.Char.UnicodeChar = L'═';
 	horizontal_symbol.Attributes = 0x1b;
@@ -19,7 +19,7 @@ void APanel::Draw()
 	vertical_symbol.Char.UnicodeChar = L'║';
 	vertical_symbol.Attributes = 0x1b;
 
-	// 1. Horizontal linesa
+	// 1. Horizontal lines
 	// 1.1 Upper line
 	Draw_Line_Horizontal(Screen_Buffer, horizontal_pos, horizontal_symbol);
 
@@ -32,9 +32,17 @@ void APanel::Draw()
 	horizontal_pos.Y_Pos = Height - 3;
 	Draw_Line_Horizontal(Screen_Buffer, horizontal_pos, horizontal_symbol);
 
-	// 2. Vertical linesa
+	// 2. Vertical lines
 	// 2.1 Left line
-	vertical_pos.Y_Pos = 1;
+	Draw_Line_Vertical(Screen_Buffer, vertical_pos, vertical_symbol);
+
+	// 2.2 Right line
+	vertical_pos.X_Pos = Width - 1;
+	Draw_Line_Vertical(Screen_Buffer, vertical_pos, vertical_symbol);
+
+	// 2.3 Middle line
+	vertical_pos.X_Pos = Width / 2;
+	vertical_pos.Length -= 2;
 	Draw_Line_Vertical(Screen_Buffer, vertical_pos, vertical_symbol);
 
 
