611834c81012147e1cbfbc3e843d28865e0148d9
700grains <60423788+700grains@users.noreply.github.com>
2023-05-28 19:29:07 +0300

    Apply a pseudographic character to draw a line

--- Panel.cpp
+++ Panel.cpp
@@ -1,4 +1,4 @@
-#include "Panel.h"
+﻿#include "Panel.h"
 // APanel
 //------------------------------------------------------------------------------------------------------------
 APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height, CHAR_INFO* screen_buffer, unsigned short screen_width)
@@ -9,12 +9,12 @@ APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width,
 void APanel::Draw()
 {
    CHAR_INFO symbol{};
-   symbol.Char.UnicodeChar = L'X';
+   symbol.Char.UnicodeChar = L'═';
    symbol.Attributes = 0x1b;
    XYPos x_y_pos(2, 1, Screen_Width, 10);
 
-   //Draw_Line_Horizontal(Screen_Buffer, x_y_pos, symbol);
-   Draw_Line_Vertical(Screen_Buffer, x_y_pos, symbol);
+   Draw_Line_Horizontal(Screen_Buffer, x_y_pos, symbol);
+   //Draw_Line_Vertical(Screen_Buffer, x_y_pos, symbol);
 
    //Show_Colors(Screen_Buffer, x_y_pos, symbol);
 }
